eb613fd27d5c753eba2700fe9617d9348d2880b6
jiajunzh <jiajunzh@alumni.cmu.edu>
2021-12-17 22:39:58 -0500

    Add function for horizontal movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * The PlayerController controls the movement of the players
+ */
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    /**
+     * The method controls the movement of the players
+     */
+    void Move()
+    {
+        // 0 for no movement, -1 for left movement and 1 for right movement
+        float horizontalDirection = Input.GetAxis("Horizontal");
+
+        if (horizontalDirection != 0)
+        {
+            rigidbody2D.velocity = new Vector2(horizontalDirection * speed, rigidbody2D.velocity.y);
+        }
+    }
+}
